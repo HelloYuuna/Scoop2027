f6a980b7029709b84d7fa68d75d6678710eabdb8
HelloYuuna <hah1236@hotmail.co.jp>
2022-09-14 16:45:58 +0900

    0914

--- src/main/java/com/example/scoop/config/auth/SecurityConfig.java
+++ src/main/java/com/example/scoop/config/auth/SecurityConfig.java
@@ -22,26 +22,30 @@ public class SecurityConfig {
 
 	   @Bean
 	   @Order(SecurityProperties.BASIC_AUTH_ORDER)
-	   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		   
-	       http
+	   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { http
 	               .csrf().disable()
 	               .headers().frameOptions().disable() 
 	               .and()
                    .authorizeRequests()
-                   .antMatchers("/", "/css/**", "/img/**",
-                           "/js/**", "/h2-console/**").permitAll()
-                   .antMatchers("/api/v1/**").hasRole(Role.
-                                                    USER.name())
+                   .antMatchers("/css/**",
+						   "/img/**",
+                           "/js/**").permitAll()
+                   .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                    .anyRequest().authenticated()
 	               .and()
-                   .logout()
-                   .logoutUrl("/logout")
-                   .logoutSuccessUrl("/").permitAll()
-	               .and()
-                   .oauth2Login()
-                   .userInfoEndpoint()
-                   .userService(customOAuth2UserService);
+				   .formLogin()						//일반적인 폼을 이용한 로그인 처리/실패 방법을 사용
+				   .loginPage("/member/loginForm")		//시큐리티에서 제공하는 기본 폼이 아닌 사용자가 만든 폼 사용
+				   .loginProcessingUrl("/member/login").permitAll()	//인증 처리를 하는 URL을 설정. 로그인 폼의 action으로 지정
+				   .usernameParameter("memberid")		//로그인폼의 아이디 입력란의 name
+				   .passwordParameter("memberpw");		//로그인폼의 비밀번호 입력란의 name
+//				   .and()
+//                   .logout()
+//                   .logoutUrl("/logout")
+//                   .logoutSuccessUrl("/").permitAll()
+//	               .and()
+//                   .oauth2Login()
+//                   .userInfoEndpoint()
+//                   .userService(customOAuth2UserService);
 		return http.build();
 	   }
 	   
---
+++ src/main/java/com/example/scoop/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.example.scoop.controller;
+
+/**
+ * projectName     :Scoop
+ * fileName        :MemberController
+ * author          :yuuna
+ * since           :2022/09/14
+ */
+public class MemberController {
+}
